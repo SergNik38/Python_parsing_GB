3b5c5a6027ace6b2584c6104645f823ec320de00
Sergey <segairk@gmail.com>
2022-01-20 21:19:09 +0800

    Lesson 5. Code fix 2.

--- homework_5/hw5.py
+++ homework_5/hw5.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-import time
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 
