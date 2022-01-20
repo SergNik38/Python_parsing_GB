3e164b97c9ead4dc0e81062389d2b8c7a99d88b7
Sergey <segairk@gmail.com>
2022-01-20 21:18:32 +0800

    Lesson 5. Code fix.

--- homework_5/hw5.py
+++ homework_5/hw5.py
@@ -1,10 +1,6 @@
-from gettext import find
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from pymongo import MongoClient
